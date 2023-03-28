9e4c0d81a856ffb83738acba2063b75ef06c79bc
ashrafulasif123 <cseasifiiuc9@gmail.com>
2023-03-28 21:19:37 +0600

    spreading cart

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
+    const handleAddToCart = props.handleAddToCart
     const { img, name, seller, ratings, quantity, price } = props.product
     return (
         <div className='product'>
@@ -12,7 +13,7 @@ const Product = (props) => {
                 <p className='product-manufacturer'>Manufacturer: {seller}</p>
                 <p>Rate: {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
